facd1357c08a4e5416cae199863f4a447d086d8c
Jonathan Hawk <jonathan@hawk.ninja>
2023-09-20 08:37:09 -0400

    feat: initial project setup

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,92 @@
-import { typescript } from 'projen';
-const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 'db-provision-pgsql',
-  projenrcTs: true,
+import { typescript, javascript } from "projen";
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+const projectUrl = "https://github.com/libreworks/db-provision-pgsql";
+
+const project = new typescript.TypeScriptProject({
+  name: "db-provision-pgsql",
+  description:
+    "Provisions a PostgreSQL database and schema with logins and grants",
+  keywords: [
+    "database",
+    "postgresql",
+    "login",
+    "grant",
+    "authentication",
+    "authorization",
+  ],
+
+  authorName: "LibreWorks Contributors",
+  authorUrl: `${projectUrl}/contributors`,
+  authorOrganization: true,
+  license: "MIT",
+
+  repository: `${projectUrl}.git`,
+  homepage: "https://libreworks.github.io/db-provision-pgsql/",
+  bugsUrl: `${projectUrl}/issues`,
+
+  deps: ["pg"],
+  devDeps: ["@jest/globals"],
+
+  minNodeVersion: "18.0.0",
+  workflowNodeVersion: "18.x",
+  tsconfig: {
+    compilerOptions: {
+      moduleResolution: javascript.TypeScriptModuleResolution.NODE16,
+      module: "node16",
+      lib: ["DOM", "ES2022"],
+      target: "es2022",
+    },
+  },
+
+  projenrcTs: true,
+  prettier: true,
+  codeCov: true,
+  docgen: true,
+
+  jestOptions: {
+    jestConfig: {
+      extensionsToTreatAsEsm: [".ts"],
+      moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+      },
+      transform: {
+        "^.+\\.ts$": new javascript.Transform("ts-jest", {
+          useESM: true,
+          tsconfig: "tsconfig.dev.json",
+        }),
+      },
+    },
+  },
+
+  majorVersion: 0,
+  defaultReleaseBranch: "main",
+  githubOptions: {
+    pullRequestLintOptions: {
+      semanticTitleOptions: { types: ["feat", "fix", "chore", "docs"] },
+    },
+  },
+  projenTokenSecret: "PROJEN_GITHUB_TOKEN",
+  autoApproveOptions: {
+    // Anyone with write access to this repository can have auto-approval.
+    allowedUsernames: [],
+  },
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ["auto-approve"],
+      schedule: javascript.UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+
+  releaseToNpm: true,
+  packageName: "@libreworks/db-provision-pgsql",
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  npmignore: ["docs"],
 });
+
+project.package.file.addOverride("type", "module");
+project.package.file.addOverride("private", false);
+project.testTask.env("NODE_OPTIONS", "--experimental-vm-modules");
+const [{ exec: projenrcCommand = "" }] = project.defaultTask!.steps;
+project.defaultTask!.reset(projenrcCommand.replace("ts-node", "ts-node-esm"));
+
 project.synth();
